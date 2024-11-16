40660c8ab629860285b39893ccce1c93ce250e07
Rufat Shikhiyev <shikhiyevrufat@gmail.com>
2024-11-16 20:28:42 +0400

    change more thing

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 from telegram import Update, ReplyKeyboardMarkup
+from bot_system import button_callback, handle_message
+from dotenv import load_dotenv
+import os
 
+
+def configure():
+    load_dotenv()
+
+TOKEN = os.getenv("token")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         ["📲 Start", "ℹ️ Help", "⚙️ Settings"],
@@ -19,9 +27,14 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(update.effective_chat.id, text, reply_markup=markup)
 
 def main():
-    application = Application.builder().token("YOUR_BOT_TOKEN").build()
+    application = Application.builder().token(TOKEN).read_timeout(60).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_message))
+
+    application.add_handler(MessageHandler(filters.ATTACHMENT, handle_message))
+
     application.run_polling()
 
 if __name__ == '__main__':
