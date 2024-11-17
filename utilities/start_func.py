30fc89b04360614db83ed4737ed1d200fbe3c6a5
Rufat Shikhiyev <shikhiyevrufat@gmail.com>
2024-11-17 13:21:31 +0400

    delete instagram

--- utilities/start_func.py
+++ utilities/start_func.py
@@ -6,9 +6,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         [InlineKeyboardButton("🖼️ Convert Image Format", callback_data='convert_image')],
         [InlineKeyboardButton("📄 Convert Documents to PDF", callback_data='convert_document')],
         [InlineKeyboardButton("📲 Generate QR Code", callback_data='generate_qr')],
-        [InlineKeyboardButton("📷 Instagram Post or Reel Download", callback_data='instagram_download')],
+        # [InlineKeyboardButton("📷 Instagram Post or Reel Download", callback_data='instagram_download')],
         [InlineKeyboardButton("🎵 Tiktok Reel Download", callback_data='tiktok_download')],
-        [InlineKeyboardButton("🎥 YouTube MP3 or MP4 Download", callback_data='youtube_download')]
+        [InlineKeyboardButton("📷 YouTube MP3 or MP4 Download", callback_data='youtube_download')]
     ]
 
     markup = InlineKeyboardMarkup(keyboard)
