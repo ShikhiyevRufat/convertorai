9048ee140b9ff3b58fae61333a2e3f9ae3844438
Rufat Shikhiyev <shikhiyevrufat@gmail.com>
2024-11-17 14:44:28 +0400

    Editing cookie

--- function/youtube_downloander.py
+++ function/youtube_downloander.py
@@ -12,7 +12,7 @@ def youtube_downloander(url, format, resolution=None):
             'outtmpl': filepath,
             'age_limit': 21,
             'verbose': True,
-            'cookiefile': 'function/youtube_cookie.txt',
+            'cookiefile': 'youtube_cookie.txt',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         }
 
