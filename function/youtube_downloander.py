db397b243fb2e5917de74f19cd4e0ddba26042d7
Rufat Shikhiyev <shikhiyevrufat@gmail.com>
2024-11-19 00:31:48 +0400

    add cookie

--- function/youtube_downloander.py
+++ function/youtube_downloander.py
@@ -3,31 +3,30 @@ import os
 
 def youtube_downloader(url, format, resolution=None):
     try:
-        # Extract video info without downloading
         video_info = youtube_dl.YoutubeDL().extract_info(url=url, download=False)
         
-        # Create a safe filename by removing invalid characters
         safe_title = "".join([c if c.isalnum() or c in (' ', '.', '_') else '' for c in video_info['title']]).rstrip()
-        safe_title = safe_title.replace(' ', '_')  # Replace spaces with underscores
+        safe_title = safe_title.replace(' ', '_')  
         filename = f"{safe_title}.{format}"
-        output_directory = os.path.join(os.getcwd(), "downloads")  # Save in a 'downloads' folder
-        os.makedirs(output_directory, exist_ok=True)  # Ensure the folder exists
+        output_directory = os.path.join(os.getcwd(), "downloads") 
+        os.makedirs(output_directory, exist_ok=True)  
         filepath = os.path.join(output_directory, filename)
 
-        # Set download options
         options = {
-            'outtmpl': filepath,  # Specify the output template
-            'quiet': False,       # Show download progress
-            'merge_output_format': format if format == 'mp4' else None
+            'outtmpl': filepath,  
+            'quiet': False,       
+            'merge_output_format': format if format == 'mp4' else None,
+            'cookies': 'youtube_cookie.txt',
+            'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         }
 
         if format == 'mp3':
             options.update({
-                'format': 'bestaudio/best[ext=m4a]',  # Audio only
-                'keepvideo': False,                  # Don't keep video for mp3
+                'format': 'bestaudio/best[ext=m4a]',  
+                'keepvideo': False,   
+
             })
         elif format == 'mp4':
-            # Adjust video resolution based on input
             format_option = 'bestvideo+bestaudio'
             if resolution == '1080p':
                 format_option = 'bestvideo[height<=1080]+bestaudio/best[height<=1080]'
@@ -37,10 +36,9 @@ def youtube_downloader(url, format, resolution=None):
                 format_option = 'bestvideo[height<=360]+bestaudio/best[height<=360]'
 
             options.update({
-                'format': format_option,  # Video + audio merge
+                'format': format_option,  
             })
 
-        # Download the video/audio
         with youtube_dl.YoutubeDL(options) as ydl:
             ydl.download([video_info['webpage_url']])
 
